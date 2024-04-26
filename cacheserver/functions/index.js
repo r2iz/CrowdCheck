b46c40305c39a369a7edee60dcaf398c6dd39b96
Pz2Or <pub@ter05.net>
2024-04-26 22:33:09 +0900

    aaaa

--- cacheserver/functions/index.js
+++ cacheserver/functions/index.js
@@ -26,3 +26,23 @@ exports.getCongestion = functions.https.onRequest(async (request, response) =>{
     response.status(200).send(congestionArray);
     response.end();
 });
+
+exports.getAdminCongestion = functions.https.onRequest(async (request, response) => {
+    const db = admin.firestore();
+    const congestionRef = db.collection("exhibition");
+    const snapshot =await congestionRef.get();
+    const exhibitionIdArray = [0,1,10,11,12,13,14,15,16,17,18,19,2,20,21,22,23,24,25,26,27,28,29,3,30,31,32,33,34,35,36,37,38,39,4,40,41,42,43,44,45,46,47,5,6,7,8,9];
+    const congestionArray = [];
+    snapshot.docs.forEach((doc, index) => {
+        congestionArray.push({
+            exhibitionId: exhibitionIdArray[index],
+            nowCongestion: doc.data().nowCongestion,
+            updatedDate: doc.data().updatedDate,
+        });
+    });
+    response.set('Access-Control-Allow-Headers', '*');
+    response.set('Access-Control-Allow-Origin', '*');
+    response.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
+    response.status(200).send(congestionArray);
+    response.end();
+});
